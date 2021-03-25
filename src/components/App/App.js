2ee5fc6f5bdd1b83afb8f605ec049b6aafe6a758
iKinsure <59565620+iKinsure@users.noreply.github.com>
2021-03-26 00:02:58 +0100

    moved logic outside App component, added kills in all directions

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,87 @@ import Board from '../Board/Board';
 import React from 'react';
 import {DATA, VIEW, INIT_DATA, BOARD_SIZE} from '../../config/enum';
 
+const isEnemy = (elem, isWhiteTurn) => {
+    return isWhiteTurn
+        ? elem === DATA.BLACK || elem === DATA.BLACK_KING
+        : elem === DATA.WHITE || elem === DATA.WHITE_KING;
+}
+
+const isAlly = (elem, isWhiteTurn) => {
+    return isWhiteTurn
+        ? elem === DATA.WHITE || elem === DATA.WHITE_KING
+        : elem === DATA.BLACK || elem === DATA.BLACK_KING;
+}
+
+const isKing = elem => elem === DATA.WHITE_KING || elem === DATA.BLACK_KING;
+
+const isMinion = elem => (elem === DATA.WHITE || elem === DATA.BLACK);
+
+const validate = (i, j) => i >= 0 && j >= 0 && i < BOARD_SIZE && j < BOARD_SIZE;
+
+const createEmptyView = () => Array(BOARD_SIZE).fill(null).map(() => Array(BOARD_SIZE).fill(VIEW.EMPTY));
+
+const createView = (i, j, data, isWhiteTurn) => {
+
+    const view = createEmptyView();
+    view[i][j] = VIEW.ACTUAL;
+
+    const directions = [
+        (i, j) => [--i, --j],
+        (i, j) => [--i, ++j],
+        (i, j) => [++i, --j],
+        (i, j) => [++i, ++j],
+    ];
+
+    // iterate and set view fields
+    directions.forEach(dir => {
+        let [di, dj] = dir(i, j);
+        while (validate(di, dj)) {
+            if (isAlly(data[di][dj], isWhiteTurn)) {
+                break;
+            } else if (isEnemy(data[di][dj], isWhiteTurn)) {
+
+                let [x, y] = dir(di, dj);
+                if (validate(x, y) && data[x][y] === DATA.EMPTY) {
+                    view[di][dj] = VIEW.KILLABLE;
+                    view[x][y] = VIEW.NECESSARY;
+                }
+                break;
+
+            } else {
+                view[di][dj] = VIEW.AVAILABLE;
+            }
+            [di, dj] = dir(di, dj);
+        }
+    });
+
+    // clean view
+    if (view.some(row => row.includes(VIEW.NECESSARY))) {
+        return view.map(row => row.map(elem => {
+            if (elem === VIEW.AVAILABLE) {
+                elem = VIEW.EMPTY;
+            }
+            return elem;
+        }));
+    } else if (isWhiteTurn && isMinion(data[i][j])) {
+        return view.map((row, di) => row.map((elem, dj) => {
+            if (i < di) {
+                elem = VIEW.EMPTY;
+            }
+            return elem;
+        }));
+    } else if (!isWhiteTurn && isMinion(data[i][j])) {
+        return view.map((row, di) => row.map((elem, dj) => {
+            if (i > di) {
+                elem = VIEW.EMPTY;
+            }
+            return elem;
+        }));
+    }
+
+    return view;
+}
+
 /**
  * Main component with game logic
  */
@@ -12,129 +93,43 @@ class App extends React.Component {
         super(props);
         this.state = {
             data: INIT_DATA,
-            view: this.resetView(),
-            whiteNext: true,
+            view: createEmptyView(),
+            isWhiteTurn: true,
             rotated: false,
         };
     }
 
-    isMoveDiagonal(i, j, di, dj) {
-        return (j + di === dj + i) || (j + i === dj + di);
-    }
-
-    resetView() {
-        return Array(BOARD_SIZE)
-            .fill(null)
-            .map(() => Array(BOARD_SIZE).fill(VIEW.EMPTY));
-    }
-
-    checkSize(i, j) {
-        return i >= 0 && j >= 0 && i < BOARD_SIZE && j < BOARD_SIZE;
-    }
-
-    check(i, j, view, fun) {
-        [i, j] = this.checkToBlock(i, j, view, fun);
-        this.checkAfter(i, j, view, fun);
-    }
-
-    checkToBlock(i, j, view, fun) {
-        [i, j] = fun(i, j);
-        while (this.checkSize(i, j) && this.state.data[i][j] === DATA.EMPTY) {
-            view[i][j] = VIEW.AVAILABLE;
-            [i, j] = fun(i, j);
-        }
-        return [i, j];
-    }
-
-    checkAfter(i, j, view, fun) {
-        if (this.checkSize(i, j)) {
-            let prevI = i;
-            let prevJ = j;
-            let elem = this.state.data[i][j];
-            if (this.state.whiteNext) {
-                if (elem === DATA.BLACK || elem === DATA.BLACK_KING) {
-                    [i, j] = fun(i, j);
-                    if (this.checkSize(i, j)) {
-                        elem = this.state.data[i][j];
-                        if (elem === DATA.EMPTY) {
-                            view[i][j] = VIEW.NECESSARY;
-                            view[prevI][prevJ] = VIEW.KILLABLE;
-                        }
-                    }
-                }
-            } else {
-                if (elem === DATA.WHITE || elem === DATA.WHITE_KING) {
-                    [i, j] = fun(i, j);
-                    if (this.checkSize(i, j)) {
-                        elem = this.state.data[i][j];
-                        if (elem === DATA.EMPTY) {
-                            view[i][j] = VIEW.NECESSARY;
-                            view[prevI][prevJ] = VIEW.KILLABLE;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    checker(i, j, functions) {
-        // calculate view
-        const view = this.resetView();
-        view[i][j] = VIEW.ACTUAL;
-        // check all directions
-        functions.forEach(f => this.check(i, j, view, f));
-        // saving as previous
-        this.prevI = i;
-        this.prevJ = j;
-        // update view
-        this.setState({
-            view: view,
-        });
-    }
-
     handleClick(i, j) {
-        const elem = this.state.data[i][j];
 
-        const upperLeft = (i, j) => [--i, --j];
-        const upperRight = (i, j) => [--i, ++j];
-        const bottomLeft = (i, j) => [++i, --j];
-        const bottomRight = (i, j) => [++i, ++j];
+        const data = this.state.data;
+        const view = this.state.view;
+        const turn = this.state.isWhiteTurn;
 
-        switch (elem) {
-            case DATA.WHITE:
-                if (this.state.whiteNext) {
-                    this.checker(i, j, [upperRight, upperLeft]);
-                }
-                break;
-            case DATA.WHITE_KING:
-                if (this.state.whiteNext) {
-                    this.checker(i, j, [upperRight, upperLeft, bottomRight, bottomLeft]);
-                }
-                break;
-            case DATA.BLACK:
-                if (!this.state.whiteNext) {
-                    this.checker(i, j, [bottomRight, bottomLeft]);
-                }
-                break;
-            case DATA.BLACK_KING:
-                if (!this.state.whiteNext) {
-                    this.checker(i, j, [upperRight, upperLeft, bottomRight, bottomLeft]);
-                }
-                break;
-            default:
-                if (this.state.view[i][j] === VIEW.AVAILABLE) {
-                    this.move(i, j, this.prevI, this.prevJ);
-                    this.setState({view: this.resetView()});
-                } else if (this.state.view[i][j] === VIEW.NECESSARY) {
-                    const obj= {
-                        i: i - (i >= this.prevI ? 1 : -1),
-                        j: j - (j >= this.prevJ ? 1 : -1),
-                    };
-                    console.log(obj);
-                    this.move(i, j, this.prevI, this.prevJ, obj);
-                    this.setState({view: this.resetView()});
-                }
-                break;
+        if (view[i][j] === VIEW.AVAILABLE) {
+
+            // TODO: check if there is necessary move available
+
+            this.move(i, j, this.previous.i, this.previous.j);
+            this.setState({view: createEmptyView()});
+
+        } else if (view[i][j] === VIEW.NECESSARY) {
+
+            this.move(i, j, this.previous.i, this.previous.j, {
+                i: i - (i >= this.previous.i ? 1 : -1),
+                j: j - (j >= this.previous.j ? 1 : -1),
+            });
+            this.setState({view: createEmptyView()});
+
+        } else if (isAlly(data[i][j], turn)) {
+
+            this.previous = {
+                i: i,
+                j: j,
+            };
+            this.setState({
+                view: createView(i, j, data, turn),
+            });
+
         }
     }
 
@@ -144,9 +139,9 @@ class App extends React.Component {
         const data = this.state.data.slice();
 
         // check if piece becomes king
-        if (this.state.whiteNext && i === 0) {
+        if (this.state.isWhiteTurn && i === 0) {
             data[i][j] = DATA.WHITE_KING;
-        } else if (!this.state.whiteNext && i === (BOARD_SIZE - 1)) {
+        } else if (!this.state.isWhiteTurn && i === (BOARD_SIZE - 1)) {
             data[i][j] = DATA.BLACK_KING;
         } else {
             data[i][j] = data[di][dj];
@@ -155,6 +150,7 @@ class App extends React.Component {
         // kill
         if (dead !== undefined) {
             data[dead.i][dead.j] = DATA.EMPTY;
+            // TODO: check if can kill again
         }
 
         // clear old field after
@@ -162,7 +158,7 @@ class App extends React.Component {
 
         this.setState({
             data: data,
-            whiteNext: !this.state.whiteNext,
+            isWhiteTurn: !this.state.isWhiteTurn,
         });
     }
 
@@ -183,7 +179,7 @@ class App extends React.Component {
                         view={this.state.view}
                         classes={this.state.rotated ? 'board rotated' : ''}
                     />
-                    <p>{this.state.whiteNext ? 'White round' : 'Black round'}</p>
+                    <p>{this.state.isWhiteTurn ? 'White round' : 'Black round'}</p>
                     <button onClick={() => this.rotate()} >
                         Rotate board
                     </button>
@@ -192,5 +188,6 @@ class App extends React.Component {
         );
     }
 }
+
 
 export default App;
