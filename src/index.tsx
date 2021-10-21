74465bce63305acbab8547037a9270bdb1670d57
iKinsure <59565620+iKinsure@users.noreply.github.com>
2021-10-22 00:25:04 +0200

    remove: vitals

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+import { StrictMode } from 'react'
+import { render } from 'react-dom'
+import { App } from './components/App/App'
+
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.getElementById('root')
+)
