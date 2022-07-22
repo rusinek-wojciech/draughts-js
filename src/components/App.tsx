c22e40574fcc26231d6896a06d7a5425cea565dc
iKinsure <59565620+iKinsure@users.noreply.github.com>
2022-07-22 04:32:22 +0200

    added footer in app

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -32,6 +32,16 @@ const App = () => {
       {isEnabledMenu && (
         <Menu playerVsPlayer={handleStartGame} playerVsAI={handleStartGame} />
       )}
+      <footer className='footer'>
+        <header>
+          <h1>How to play?</h1>
+          <p>
+            Your goal is to eliminate enemy pawns. Shadow on the edge of board
+            shows which player has turn. Use <i>right click</i> to select pawn.
+            Then use <i>left click</i> to select one of available move.
+          </p>
+        </header>
+      </footer>
     </div>
   )
 }
